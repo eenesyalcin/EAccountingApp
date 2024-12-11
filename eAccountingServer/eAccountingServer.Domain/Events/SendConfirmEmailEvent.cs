09f5046d549d6ba3e6fdbcc20d17db61a2b68199
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-12-12 00:35:03 +0300

    Confirmation email sending and approval processes for the backend have been completed.

---
+++ eAccountingServer/eAccountingServer.Domain/Events/SendConfirmEmailEvent.cs
@@ -0,0 +1,32 @@
+﻿using eAccountingServer.Domain.Entities;
+using FluentEmail.Core;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace eAccountingServer.Domain.Events;
+
+public sealed class SendConfirmEmailEvent(
+    UserManager<AppUser> userManager,
+    IFluentEmail fluentEmail) : INotificationHandler<AppUserEvent>
+{
+    public async Task Handle(AppUserEvent notification, CancellationToken cancellationToken)
+    {
+        AppUser? appUser = await userManager.FindByIdAsync(notification.UserId.ToString());
+        if (appUser is not null)
+        {
+            await fluentEmail
+                .To(appUser.Email)
+                .Subject("Mail Onayı")
+                .Body(CreateBody(appUser), true)
+                .SendAsync(cancellationToken);
+        }
+    }
+
+    private string CreateBody(AppUser appUser)
+    {
+        string body = $@"Mail adresinizi onaylamak için aşaağıdaki linke tıklayınız.
+<a href='http://localhost:4200/confirm-email/{appUser.Email}' target='_blank'>Maili onaylamak için tıklayınız.</a>";
+
+        return body;
+    }
+}
